db03a0974272204b23feab4a968500f0fb228e4b
Ryosuke Hana <ryosuke.san.hana@gmail.com>
2024-10-11 15:39:19 -0700

    :construction: auto saves render

--- __init__.py
+++ __init__.py
@@ -13,14 +13,19 @@ bl_info = {
 
 
 import bpy
+import os
+from datetime import datetime
+from bpy.app.handlers import persistent
 from bpy.props import (IntProperty,
                        FloatProperty,
                        EnumProperty,
                        PointerProperty,
+                       BoolProperty,
                        )
 from bpy.types import (
                        PropertyGroup,
                        )
+from bpy.path import basename
 
 # ------------------------------------------------------------------------
 #    Utility Functions
@@ -63,6 +68,11 @@ class GI_SceneProperties(PropertyGroup):
         min = 1,
         max = 1000
         )
+    should_auto_save: BoolProperty(
+        name = "Auto Save",
+        description = "Auto saves image with correct dimensions and DPI. Saves in Blender file's folder.",
+        default = False,
+        )
 
 # UI Panel
 def draw_func(self, context):
@@ -77,6 +87,7 @@ def draw_func(self, context):
         row.prop(dpi_props, "width")
         row.prop(dpi_props, "height")
         row.prop(dpi_props, "dpi")
+        row.prop(dpi_props, "should_auto_save")
 
         row.operator("dpi_settings.sync_size")
 
@@ -103,6 +114,50 @@ class DPI_SETTINGS_sync_size(bpy.types.Operator):
         bpy.context.scene.render.resolution_y = round(height_px);
         return {"FINISHED"}
 
+@persistent
+def auto_save_render(scene):
+    print("auto saving...")
+    dpi_props = scene.dpi_props
+    width = dpi_props.width
+    height = dpi_props.height
+    dpi = dpi_props.dpi
+    should_auto_save = dpi_props.should_auto_save
+
+    if not should_auto_save or not bpy.data.filepath:
+        return
+
+    render = scene.render
+    extension = render.image_settings.file_format
+
+    # Generate a file path
+    # We pick the same folder as current Blender file and save inside a `/renders` folder
+    # And we use the Blender filename as the basis for the image name
+    blender_filepath = bpy.data.filepath
+    blender_file_dir = os.path.dirname(blender_filepath)
+    image_dir = os.path.join(blender_file_dir, "renders")
+    image_base_name = basename(bpy.data.filepath).rpartition('.')[0]
+    
+    # We generate the current date to make a unique identifier for file
+    now = datetime.now() # current date and time
+    date_time = now.strftime("%m-%d-%Y-%H-%M-%S")
+
+    # We also append the file info for quick ref
+    image_size_info = width + "in-" + height + "in-" + dpi
+
+    # Merge them all together
+    image_name = image_base_name + "_" + image_size_info + "_" + date_time + "." + extension
+    image_final_path = os.path.join(image_dir, image_name)
+
+    print("blender_filepath", blender_filepath)
+    print("blender_file_dir", blender_file_dir)
+    print("image path", image_final_path)
+
+    # Save image
+    image = bpy.data.images['Render Result']
+    image.save_render(image_final_path, scene=None)
+
+
+
 # Load/unload addon into Blender
 classes = (
     GI_SceneProperties,
@@ -117,12 +172,14 @@ def register():
 
     bpy.types.Scene.dpi_props = PointerProperty(type=GI_SceneProperties)
     bpy.types.RENDER_PT_format.append(draw_func)
+    bpy.app.handlers.render_post.append(auto_save_render)
 
 def unregister():
     from bpy.utils import unregister_class
     for cls in reversed(classes):
         unregister_class(cls)
     bpy.types.RENDER_PT_format.remove(draw_func)
+    bpy.app.handlers.render_post.remove(auto_save_render)
 
 
 if __name__ == "__main__":
